894c93d9e6c1a719b6cba0164a4dddb34680ca7d
Brian Clapper <bmc@clapper.org>
2010-09-17 08:16:42 -0400

    Updated to 0.3. Adjusted docs to reflect that SBTPlugins is now on scala-tools.org

--- project/build/SBTPlugins.scala
+++ project/build/SBTPlugins.scala
@@ -16,12 +16,10 @@ class SBTPluginsProject(info: ProjectInfo)
     lazy val markdown = project("markdown")
     lazy val editsource = project("editsource")
 
-    // "publish" will prompt (via a Swing pop-up) for the username and
-    // password.
-    val publishTo = Resolver.sftp("clapper.org Maven Repo",
-                                  "maven.clapper.org",
-                                  "/var/www/maven.clapper.org/html")
+    lazy val publishTo = "Scala Tools Nexus" at
+        "http://nexus.scala-tools.org/content/repositories/releases/"
+    Credentials(Path.userHome / "src" / "mystuff" / "scala" /
+                "nexus.scala-tools.org.properties", log)
 
     override def managedStyle = ManagedStyle.Maven
-
 }
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -2,9 +2,6 @@ import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info)
 {
-    // Managed dependencies that are used by the project file itself.
-    // Putting them here allows them to be imported in the project class.
-
     val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public"
     val snug_repo = "uk-releases" at "http://www2.ph.ed.ac.uk/maven2/"
 
